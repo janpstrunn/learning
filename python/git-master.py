0ab80d69c9b8b0e5638a694b2fb2c91e36bc645c
Janpstrunn <janpstrunn@particular.slmail.me>
2024-10-20 16:49:11 -0300

    lsd obsidian vaults

--- python/git-master.py
+++ python/git-master.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python3
 
+## Modules ###
+
 import tkinter as tk
 from tkinter import simpledialog
 import os
 import subprocess
 
-# Directories
+###############
+
+### Directories ###
 
 pandora = "/run/media/janpstrunn/Pandora/"
 obsidian = "Obsidian/"
 forgejo = "Forgejo/"
-p_j = os.path.join(pandora, forgejo)
+
+####################
 
 print("Available repos:\n")
-subprocess.run(["lsd", p_j], text=True, check=True)
-
+subprocess.run(["lsd", pandora + forgejo], text=True, check=True)
+subprocess.run(["lsd", pandora + obsidian], text=True, check=True)
 git_repo_dir = input("\nWhat git repo do you want to commit?\n")
 
 obsidian_repos = {"YGGDRASIL", "LAPLACE", "OUROBOROS"}
 
-if git_repo_dir.lower() in obsidian_repos:
+if git_repo_dir in obsidian_repos:
     full_obsidian_path = os.path.join(pandora, obsidian, git_repo_dir + "/")
 
     root = tk.Tk()
@@ -35,7 +40,7 @@ if git_repo_dir.lower() in obsidian_repos:
         print(f"An error occurred: {e}")
 
 else:
-    full_path = os.path.join(p_j, git_repo_dir + "/")
+    full_path = os.path.join(pandora, forgejo, git_repo_dir + "/")
 
     root = tk.Tk()
     root.withdraw()
